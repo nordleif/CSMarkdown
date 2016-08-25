521244109a9f05d978aafe3d08c2f139c913af92
Nicholai Axelgaard <nichoaxel@hotmail.com>
2016-08-25 09:50:39 +0200

    Test

--- CSMarkdown.Core/Scripting/DefaultScriptContext.cs
+++ CSMarkdown.Core/Scripting/DefaultScriptContext.cs
@@ -69,6 +69,7 @@ namespace CSMarkdown.Scripting
                 data = new DataTable();
                 data = FillEmptyDataTable();
             }
+
             if (options == null)
             {
                 options = CreateDefaultChartOptions(data);
