ff1921c4577bb8b45ae821cc8dee2038b675d70e
Axelfarm <nichoaxel@hotmail.com>
2016-08-29 10:00:13 +0200

    Addition to 157c5a53

--- CSMarkdown.Tests/CSMarkdownRendererTests.cs
+++ CSMarkdown.Tests/CSMarkdownRendererTests.cs
@@ -36,7 +36,6 @@ namespace CSMarkdown.Tests
         [TestCase("markdown_017.smd", RenderOutput.Html)]
         [TestCase("markdown_018.smd", RenderOutput.Html)]
         [TestCase("markdown_019.smd", RenderOutput.Html)]
-        [TestCase("markdown_020.smd", RenderOutput.Html)]
 
         [TestCase("markdown_display_error.smd", RenderOutput.Html)]
         [TestCase("markdown_display_message.smd", RenderOutput.Html)]
