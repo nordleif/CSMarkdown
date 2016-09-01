4f3e7e31dca5419dd2a0e2129aaa7acc97da0979
Axelfarm <nichoaxel@hotmail.com>
2016-09-01 13:09:29 +0200

    Added a way to see what the console output would be

--- CSMarkdown.Tests/CSMarkdownRendererTests.cs
+++ CSMarkdown.Tests/CSMarkdownRendererTests.cs
@@ -120,6 +120,7 @@ namespace CSMarkdown.Tests
         [TestCase("-r -s markdown_2_legends.smd -n \"new name of output with whitespaces\" -i ../../Documents -o C:/temp -t html", TestName = "App: new name of output file with whitespaces")]
         [TestCase("-r -s markdown_yaml_params.smd -n FileMadeUsingParams -i ../../Documents -o C:/temp -t html -p \"from = 2015-12-29, to= 2016-12-29, tag = foo, boo\"", TestName = "App: using params")]
         [TestCase("-r -s markdown_2_legends.smd -n HubbaPDF -i ../../Documents -o C:/temp -t pdf", TestName = "App: new name and pdf as output")]
+        [TestCase("/?", TestName = "App: get /? for CSMarkdown.exe")]
         [Test]
         public void MarkdownAppTest(string args)
         {
@@ -130,18 +131,66 @@ namespace CSMarkdown.Tests
             process.StartInfo.Arguments = args;
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.RedirectStandardOutput = true;
             process.Start();
+            StreamReader errorReader = process.StandardError;
+            string errorOutput = errorReader.ReadToEnd();
 
-            StreamReader reader = process.StandardError;
-            string output = reader.ReadToEnd();
+
+            //Writes the output, which normally would have been shown in the Console
+            StreamReader outputReader = process.StandardOutput;
+            string consoleOutput = outputReader.ReadToEnd();
+            string consoleOutputPath = "C:/temp/CSMarkdownOutput.txt";
+            string currentContent = String.Empty;
+            if (File.Exists(consoleOutputPath))
+                currentContent = File.ReadAllText(consoleOutputPath);
+            File.WriteAllText(consoleOutputPath, DateTime.Now + " - " + TestContext.CurrentContext.Test.Name + "\r\n" + consoleOutput + "\r\n" + currentContent);
+            
+
             process.WaitForExit();
             process.Close();
 
-            if (output != "")
+            if (!string.IsNullOrEmpty(errorOutput))
             {
-                if (output != "Qt: Could not initialize OLE (error 80010106)\r\n")
-                    throw new Exception(output);
+                if (errorOutput != "Qt: Could not initialize OLE (error 80010106)\r\n")
+                    throw new Exception(errorOutput);
             }
         }
+
+        //[Test]
+        //public void GetCSMarkdownAppArguementOptions()
+        //{
+        //    string outputPath = "C:/temp/CSMarkdownArguementOptions.txt";
+
+        //    string exeFilePath = AppDomain.CurrentDomain.BaseDirectory;
+        //    exeFilePath = exeFilePath.Remove(exeFilePath.Length - 27) + "CSMarkdown.App\\bin\\Debug\\CSMarkdown.exe";
+        //    Process process = new Process();
+        //    process.StartInfo.FileName = exeFilePath;
+        //    process.StartInfo.Arguments = "/?";
+        //    process.StartInfo.UseShellExecute = false;
+        //    process.StartInfo.RedirectStandardError = true;
+        //    process.StartInfo.RedirectStandardOutput = true;
+        //    process.Start();
+
+        //    StreamReader errorReader = process.StandardError;
+        //    string errorOutput = errorReader.ReadToEnd();
+        //    StreamReader outputReader = process.StandardOutput;
+        //    string output = outputReader.ReadToEnd();
+
+        //    StreamWriter writer = new StreamWriter(outputPath);
+        //    if (!string.IsNullOrWhiteSpace(errorOutput))
+        //    {
+        //        writer.WriteLine(errorOutput);
+        //        writer.WriteLine();
+        //    }
+        //    if (!string.IsNullOrWhiteSpace(output))
+        //    writer.WriteLine(output);
+        //    writer.Close();
+
+        //    process.WaitForExit();
+        //    process.Close();
+
+        //    Process.Start(outputPath);
+        //}
     }
 }
