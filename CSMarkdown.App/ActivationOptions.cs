e094c1d6003b29109d9cf6e0872aceb61a369562
Axelfarm <nichoaxel@hotmail.com>
2016-10-10 09:05:42 +0200

    Minor adjustments of the descriptions for the ActivationsOptions in the .App

--- CSMarkdown.App/ActivationOptions.cs
+++ CSMarkdown.App/ActivationOptions.cs
@@ -9,31 +9,31 @@ namespace CSMarkdown.App
 {
     internal class ActivationOptions
     {
-        [SwitchArgument('w', "webapp", false, Description = "Starts the web app")]
+        [SwitchArgument('w', "webapp", false, Description = "Starts the web app.")]
         public bool WebApp { get; set; }
 
-        [SwitchArgument('r', "render", false, Description = "Render report")]
+        [SwitchArgument('r', "render", false, Description = "Render report.")]
         public bool Render { get; set; }
 
-        [SwitchArgument('f', "flattenhtml", true, Description = "Use when you don't want the html to be flattened. Default is true")]
+        [SwitchArgument('f', "flattenhtml", true, Description = "Use when you don't want the html to be flattened. Default is true.")]
         public bool FalttenHtml { get; set; }
 
-        [ValueArgument(typeof(string), 's', LongName = "smdfile", Description = "Name of the .smd file to use")]
+        [ValueArgument(typeof(string), 's', LongName = "smdfile", Description = "Name of the .smd file to use.")]
         public string SmdFileName { get; set; }
 
-        [ValueArgument(typeof(string), 'n', LongName = "newfilename", Description = "Name of the output file. If name consists of white spaces, use \"[name]\" around the name")]
+        [ValueArgument(typeof(string), 'n', LongName = "newfilename", Description = "Name of the output file. If name consists of white spaces, use \"[name]\" around the name.")]
         public string NewFileName { get; set; }
 
-        [ValueArgument(typeof(string), 'i', LongName = "inputpath", Description = "Holds the path of the smd file to use")]
+        [ValueArgument(typeof(string), 'i', LongName = "inputpath", Description = "Holds the path of the smd file to use.")]
         public string InputPath { get; set; }
 
-        [ValueArgument(typeof(string), 'o', LongName = "outputpath", Description = "Holds the path of where to put the output file")]
+        [ValueArgument(typeof(string), 'o', LongName = "outputpath", Description = "Holds the path of where to put the output file.")]
         public string OutputPath { get; set; }
 
-        [ValueArgument(typeof(string), 't', LongName = "outputtype", Description = "Define which type the output document should be")]
+        [ValueArgument(typeof(string), 't', LongName = "outputtype", Description = "Define which type the output document should be.")]
         public string OutputType { get; set; }
 
-        [ValueArgument(typeof(string), 'p', LongName = "parameters", Description = "Holds the parameters for the report. If the argument with parameters contains whitespaces, then use \"[parameters]\" to encapsulate", Example = "from=2016-04-01, to=2016-01-03, tag=NameTag")]
+        [ValueArgument(typeof(string), 'p', LongName = "parameters", Description = "Holds the parameters for the report. If the argument with parameters contains whitespaces, then use \"[parameters]\" to encapsulate.", Example = "from=2016-11-25, to=2016-11-28, tag=NameTag")]
         public string Parameters { get; set; }
     }
 }
